116e04de570b8b181ef7f232bf2cd86359e6730c
Vana Nikolaidou <v.nikolaidou@spectre.gr>
2024-12-13 10:55:09 +0200

    Solution commit

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+// POSTS API
+// https://jsonplaceholder.typicode.com/posts
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Post {
+  id: string
+  title: string
+  body: string
 }
 
-export default App
+interface PostCardProps {
+  post: Post
+}
+
+function PostCard({post}: PostCardProps) {
+
+  const postBody = 
+    post.body.length > 200 ? 
+      post.body.substring(0,200) + "..." :
+      post.body;
+     
+  return <div style={{ display: 'flex', flexDirection: 'column'}}>
+    {/*TODO: Add an image here 200x200 by keeping to enhance card just UI -> "https://picsum.photos/200/300" */}
+    <img src={"https://picsum.photos/200/300"} alt="img" style={{width: "200px", height: "200px", objectFit: "cover"}}/>
+    <div>
+    {/* TODO: Add Post Title */}
+    {post.title}
+    </div>
+    <div>
+      {/* TODO: Add Post body with 200 chars max and add ... at the end */}
+      {postBody}
+    </div>
+  </div>
+}
+
+export default function App() {
+  const [posts, setPosts] = useState<Post[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [searchText, setSearchText] = useState("");
+
+  // TODO: Fetch Posts
+
+  const fetchPosts = async () => {
+    setIsLoading(true);
+    try {
+      const postResponse = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const postData = await postResponse.json();
+      setPosts(postData);
+    }
+    catch (error) {
+      console.log("error", error);
+    }
+    finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  // TODO: Filter posts by post title based on the given query
+  const filterByTitle = posts.filter((post) => 
+    post.title.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  return (
+    <div className='App'>
+      {/*  TODO: Implement query controller */}
+      <input placeholder="Search post" 
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        {/* TODO: Render the PostCard for each post */}
+        {isLoading ? (
+          <h1>Loading...</h1>
+        ) : 
+        (filterByTitle.map((post) => <PostCard key={post.id} post={post}/>))
+        }
+      </div>
+
+    </div>
+  );
+}
